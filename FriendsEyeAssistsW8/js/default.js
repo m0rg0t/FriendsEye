c5ce9a4b797d9f5cb837cb9fd55d2b81509eda6e
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-11-09 13:59:36 +0400

    Init projects in solution

---
+++ FriendsEyeAssistsW8/js/default.js
@@ -0,0 +1,48 @@
+﻿// Основные сведения о шаблоне концентратора см. в следующей документации:
+// http://go.microsoft.com/fwlink/?LinkID=286574
+(function () {
+    "use strict";
+
+    var activation = Windows.ApplicationModel.Activation;
+    var app = WinJS.Application;
+    var nav = WinJS.Navigation;
+    var sched = WinJS.Utilities.Scheduler;
+    var ui = WinJS.UI;
+
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: Это приложение было вновь запущено. Инициализируйте
+                // приложение здесь.
+            } else {
+                // TODO: Это приложение вновь активировано после приостановки.
+                // Восстановите состояние приложения здесь.
+            }
+
+            nav.history = app.sessionState.history || {};
+            nav.history.current.initialPlaceholder = true;
+
+            // Оптимизация загрузки приложений и выполнение запланированных задач с высоким приоритетом во время отображения экрана-заставки.
+            ui.disableAnimations();
+            var p = ui.processAll().then(function () {
+                return nav.navigate(nav.location || Application.navigator.home, nav.state);
+            }).then(function () {
+                return sched.requestDrain(sched.Priority.aboveNormal + 1);
+            }).then(function () {
+                ui.enableAnimations();
+            });
+
+            args.setPromise(p);
+        }
+    });
+
+    app.oncheckpoint = function (args) {
+        // TODO: Это приложение будет приостановлено. Сохраните все состояния,
+        // которые должны сохраняться во время приостановки. Если необходимо 
+        // завершить асинхронную операцию, прежде чем приложение 
+        // будет приостановлено, call args.setPromise().
+        app.sessionState.history = nav.history;
+    };
+
+    app.start();
+})();
---
+++ FriendsEyeAssistsW8/pages/item/item.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/item/item.html", {
+        // Эта функция вызывается каждый раз, когда пользователь переходит на данную страницу. Она
+        // заполняет элементы страницы данными приложения.
+        ready: function (element, options) {
+            var item = Data.resolveItemReference(options.item);
+            element.querySelector(".titlearea .pagetitle").textContent = item.title;
+
+            // TODO: Инициализируйте страницу здесь.
+        }
+    });
+})();
