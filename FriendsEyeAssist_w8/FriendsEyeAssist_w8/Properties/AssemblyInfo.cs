ebe81dc63fcbf53df29d4d8b2f55032dbb3074e7
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-11-10 02:43:09 +0400

    Init windows 8 commit

---
+++ FriendsEyeAssist_w8/FriendsEyeAssist_w8/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения
+// сопоставленные со сборкой.
+[assembly: AssemblyTitle("FriendsEyeAssist_w8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FriendsEyeAssist_w8")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// с помощью знака "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
